03c31279bdf35acbf6d7672627fa3d9ee7404965
Brandon <bkan0458@gmail.com>
2020-06-01 09:54:13 -0700

    Initial Commit

---
+++ Open Kitchen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Open Kitchen
+//
+//  Created by Brandon Kan  on 2020-06-01.
+//  Copyright © 2020 Brandon Kan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
